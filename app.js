8401bbfc5e86854ae4f8fc885f8d4a76f3436076
peter <krothu@outlook.com>
2024-02-14 15:29:41 +0000

    added cors

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 ﻿const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(cors());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
